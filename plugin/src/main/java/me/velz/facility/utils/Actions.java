e69eaf669cecc47cf6ced2f98e333ec5adb7d822
Kevin <kevin.velz@gmail.com>
2018-12-30 03:00:33 +0100

    Add Placeholder Support to Actions

--- plugin/src/main/java/me/velz/facility/utils/Actions.java
+++ plugin/src/main/java/me/velz/facility/utils/Actions.java
@@ -1,21 +1,28 @@
 package me.velz.facility.utils;
 
+import me.velz.facility.Facility;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 public class Actions {
-    
+
     public static void run(Player player, String string) {
         String[] act = string.split(">");
         String action = act[0];
         String message = act[1];
-        if(action.equalsIgnoreCase("message")) {
+        if (Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")) {
+            message = Facility.getInstance().getImplementations().getPlaceholderapi().replace(player, message);
+        }
+        if (action.equalsIgnoreCase("message")) {
             player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
         }
-        if(action.equalsIgnoreCase("command")) {
+        if (action.equalsIgnoreCase("command")) {
             Bukkit.dispatchCommand(player, message);
         }
+        if (action.equalsIgnoreCase("broadcast")) {
+            Bukkit.broadcastMessage(message);
+        }
     }
-    
+
 }

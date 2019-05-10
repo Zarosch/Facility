3e67a33da370c072cd281eae5c2621b39cf445d0
Zarosch <kevin.velz@gmail.com>
2019-05-10 20:21:57 +0200

    :star: added /homelist <player>

--- plugin/src/main/java/me/velz/facility/commands/HomeListCommand.java
+++ plugin/src/main/java/me/velz/facility/commands/HomeListCommand.java
@@ -3,6 +3,7 @@ package me.velz.facility.commands;
 import me.velz.facility.Facility;
 import me.velz.facility.database.DatabasePlayer;
 import me.velz.facility.utils.MessageUtil;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,7 +16,7 @@ public class HomeListCommand implements CommandExecutor {
     public HomeListCommand(Facility plugin) {
         this.plugin = plugin;
     }
-    
+
     @Override
     public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
         if (!cs.hasPermission("facility.command.homelist")) {
@@ -23,23 +24,43 @@ public class HomeListCommand implements CommandExecutor {
             return true;
         }
 
+        if (args.length == 1) {
+            if (!cs.hasPermission("facility.command.homelist.other")) {
+                cs.sendMessage(MessageUtil.PREFIX.getLocal() + MessageUtil.ERROR_NOPERMISSIONS.getLocal());
+                return true;
+            }
+        }
+
         if (!(cs instanceof Player)) {
             cs.sendMessage(MessageUtil.PREFIX.getLocal() + MessageUtil.ERROR_PLAYERONLY.getLocal());
             return true;
         }
 
-        final Player player = (Player) cs;
-        final DatabasePlayer dbPlayer = plugin.getDatabase().getUser(player.getUniqueId().toString());
-        if (dbPlayer.getHomes().isEmpty()) {
-            cs.sendMessage(MessageUtil.PREFIX.getLocal() + MessageUtil.HOME_NOHOMES.getLocal());
-            return true;
-        }
-        String list = "";
-        for (String home : dbPlayer.getHomes().keySet()) {
-            list = list + home + ",";
-        }
-        list = list.substring(0, list.length() - 1);
-        cs.sendMessage(MessageUtil.PREFIX.getLocal() + MessageUtil.HOME_LIST.getLocal().replaceAll("%list", list));
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+            @Override
+            public void run() {
+                final Player player = (Player) cs;
+                final String uuid;
+
+                if (args.length == 1) {
+                    uuid = plugin.getDatabase().getUUID(args[0]);
+                } else {
+                    uuid = player.getUniqueId().toString();
+                }
+
+                final DatabasePlayer dbPlayer = plugin.getDatabase().getUser(player.getUniqueId().toString());
+                if (dbPlayer.getHomes().isEmpty()) {
+                    cs.sendMessage(MessageUtil.PREFIX.getLocal() + MessageUtil.HOME_NOHOMES.getLocal());
+                } else {
+                    String list = "";
+                    for (String home : dbPlayer.getHomes().keySet()) {
+                        list = list + home + ",";
+                    }
+                    list = list.substring(0, list.length() - 1);
+                    cs.sendMessage(MessageUtil.PREFIX.getLocal() + MessageUtil.HOME_LIST.getLocal().replaceAll("%list", list));
+                }
+            }
+        });
         return true;
     }
 
